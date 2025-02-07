faf44deb325e12fbfd921ff4e3c2fd05c0a755b3
Nitesh Shukla <nitesh.shukla18@gmail.com>
2025-02-07 18:49:47 +0530

    New scenario

--- src/practice/PracClass1.java
+++ src/practice/PracClass1.java
@@ -2,6 +2,7 @@ package practice;
 
 public class PracClass1 {
 	public static void main(String[] args) {
-		System.out.println("Hello_Changes");
+		System.out.println("Hello_Changes_same line");
+		System.out.println("Hello_Changes1");
 	}
 }
