388e4aa3dff5e9e3ae3de86e45facfade516ebc4
Nitesh Shukla <nitesh.shukla18@gmail.com>
2025-02-07 18:42:29 +0530

    New Chnages push

--- src/practice/PracClass2.java
+++ src/practice/PracClass2.java
@@ -2,7 +2,6 @@ package practice;
 
 public class PracClass2 {
 	public static void main(String[] args) {
-		System.out.println("Hello_Changes2");
-		System.out.println("New Changes_2");
+		System.out.println("Hello_Changesforthesecondscenario");
 	}
 }
